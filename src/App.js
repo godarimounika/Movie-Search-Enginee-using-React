ae77a049399655d490efc30e7058838aab5ba2f1
Godari Mounika <godarimounika02@gmail.com>
2023-11-16 22:46:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,73 @@
- import React from "react"
- import { useState } from "react"
- import "./App.css"
- function App() {
-  const [seconds, setSeconds] = useState(0)
+import React,{useEffect, useState} from "react";
+import axios from 'axios'
+import "./App.css"
+  
+
+ function App(){
+    const[popularMovies,setPopularMovies] = useState([])
+    const[movieName,setMovieName]=useState("")
+    const[searchedMovies,setSearchedMovies]=useState([])
+
+    useEffect(()=>{
+axios.get("https://api.themoviedb.org/3/movie/popular?api_key=cfe422613b250f702980a3bbf9e90716")
+.then((res)=>{
+    setPopularMovies(res.data.results)
+}).catch((err)=>{
+    console.log(err)
+})
+    },[])
+
+    useEffect(()=>{
+       
+        axios.get( `https://api.themoviedb.org/3/search/movie?query=${movieName}&api_key=cfe422613b250f702980a3bbf9e90716`)
+        .then((res)=>{
+            setSearchedMovies(res.data.results)
+        })
+    },[movieName])
+  
 
 
-  const startTimer = () => {
-    setInterval(() => {
-      setSeconds(seconds => seconds + 1)
-  }, 1000)
 
-  }
+    return(
 
-  const stopTimer = () => {
-
-    clearInterval(setSeconds(0))
-        document.querySelector('#counter').remove()
-  }
-
-  const currentCount = seconds
-
-  return (
-
-      <div className="counter-container">
-         <h1 id="counter">0:0:{currentCount} </h1>
-          <button className="start-button" onClick={startTimer}>start</button>
-          <button className="stop-button" onClick={stopTimer}>stop</button>
-         
-      </div>
-  )
-}
-export default   App;
+        <>
+       
+       <div style={{textAlign:"center"}}>
+       <h1 style={{color:"wheat"}}>Movie Search Enginee using React</h1>
+        <input
+          value={movieName}
+          onChange={(e)=>{
+   setMovieName(e.target.value)
+          }}
+        placeholder="Search for Movie Title..."/>
+       <div id="Movie-Wrapper">
+     
+             {movieName =="" ? popularMovies.map((item,i)=>{
+                return(<div className="Movie-Cards ">
+                <img 
+                className="Movie-thumbnail"
+                src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${item.poster_path}`}
+                />
+                <p><b>Title:</b>{item.title}</p>
+                <p><b>Rating:</b>{item.vote_average}</p>
+                <p><b>Release Year:</b>{item.release_date}</p>
+                </div>)}):searchedMovies.map((item,i)=>{
+                return(<div className="Movie-Cards ">
+                <img 
+                className="Movie-thumbnail"
+                src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${item.poster_path}`}
+                />
+                <p><b>Title:</b>{item.title}</p>
+                <p><b>Rating:</b>{item.vote_average}</p>
+                <p><b>Release Year:</b>{item.release_date}</p>
+                </div>)})}
+               
+       </div>
+           
+       </div>
+            
+   
+        </>
+    )}
+ 
+ export default App;
